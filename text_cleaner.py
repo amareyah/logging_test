d3420521432bf56687a61c5b02e05816a64a12b7
Vakhtang Khukhunashvili <vkhukhunashvili@evex.ge>
2019-05-06 14:21:52 +0400

    added new class text_cleaner

---
+++ text_cleaner.py
@@ -0,0 +1,15 @@
+import csv
+
+data = [('IBM','content text 1','2017-08-28'),
+        ('IBM','content text 2','2017-03-19'),
+        ('IBM','content text 3','2017-07-22')]
+
+
+class Parser(object):
+
+    def write_to_csv(file_path:str,data:iterable):
+        with open(file_path,'a') as csvFile:
+            writer = csv.writer(csvFile, delimiter='|', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.writerows(data)
+            csvFile.close()
+
